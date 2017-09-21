dd2549dfcff4a2130a140d2017d198d274c3868c
Eric Herot <400447+eherot@users.noreply.github.com>
2017-09-21 17:50:04 -0400

    Check for private_propagating_vgws before defining private route tables
    I'm not sure if this is exactly the right thing to be checking for but I know that without this or `enable_nat_gateway` set there are not likely to be any private subnets to check for. Perhaps we should use `var.enable_nat_gateway` instead?

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_route" "public_internet_gateway" {
 # Private routes
 #################
 resource "aws_route_table" "private" {
-  count = "${length(var.azs)}"
+  count = "${length(var.private_propagating_vgws) > 0 ? length(var.azs) : 0}"
 
   vpc_id           = "${aws_vpc.this.id}"
   propagating_vgws = ["${var.private_propagating_vgws}"]
