ec1233bb3d820f3c5d8694791851ca54441f4dfd
Eric Herot <400447+eherot@users.noreply.github.com>
2017-09-28 10:48:25 -0400

    Use enable_nat_gateway (rather than private_propagating_vgws) to determine if we need private routes

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_route" "public_internet_gateway" {
 # Private routes
 #################
 resource "aws_route_table" "private" {
-  count = "${length(var.private_propagating_vgws) > 0 ? length(var.azs) : 0}"
+  count = "${var.enable_nat_gateway ? length(var.azs) : 0}"
 
   vpc_id           = "${aws_vpc.this.id}"
   propagating_vgws = ["${var.private_propagating_vgws}"]
